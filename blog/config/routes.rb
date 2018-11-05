6a27ca73fb724e8fa5ec12ee1e57e46ad5c1a465
Ketzenkrad <34973875+Ketzenkrad@users.noreply.github.com>
2018-11-05 17:17:29 +0300

    Удалены дубликаты роутов
    Так же удалю и в Ubuntu

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_page/about'
-  get 'static_page/help'
-	root 'home#index'
+root 'home#index'
   get 'home/index'
   get 'static_page/about'
   get 'static_page/help'
